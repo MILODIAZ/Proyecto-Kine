3b6d86a68458e1f62ad668b6b709fff0a8e1482b
MILODIAZ <milodiaz1995@gmail.com>
2022-11-20 15:26:27 -0300

    commit

--- proyecto kine/js/script.js
+++ proyecto kine/js/script.js
@@ -12,6 +12,7 @@ window.addEventListener('click', onClick);
 function ShowSideNav(){
 	console.log("Function called");
 	document.getElementById("sidebar").style.right="0px";
+	document.getElementById("sidebar").style.width="250px";
 	document.getElementById("SideNavButton").disabled=true;
 	document.getElementById("boton-hamburguesa").style.opacity="0";	
 	console.log("SideNav Deploid");	
@@ -20,6 +21,7 @@ function ShowSideNav(){
 function HideSideNav(){
 	
 	document.getElementById("sidebar").style.right="-250px";
+	document.getElementById("sidebar").style.width="0px";
 	document.getElementById("SideNavButton").disabled=false;
 	document.getElementById("boton-hamburguesa").style.opacity="1";	
 	console.log("SideNav Hidden");
